e7c3ca2baf32fbc389722533aa6a6dfe0f94a1d0
Rafael Batista <rafael@devbatista.com>
2025-05-26 18:16:56 -0300

    adicionando migration com uuid

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: {
+    admin: 0,
+    attendant: 1,
+    client: 2
+  }
+end
