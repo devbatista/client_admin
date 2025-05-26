f79108d71ec91c10fe51e0b10d22da593b431fda
Rafael Batista <rafael@devbatista.com>
2025-05-26 19:24:44 -0300

    ajustando validações da model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,3 +1,9 @@
 class Client < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :cpd_cnpj, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :phone, presence: true, format: { with: /\A\(?\d{2}\)?\s?\d{4,5}-?\d{4}\z/ }
+  validates :address, presence: true
 end
