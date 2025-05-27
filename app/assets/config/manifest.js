83071afc93c7266cc0aaa6e0f30bca3cf2fe0c57
Rafael Batista <rafael@devbatista.com>
2025-05-27 14:17:56 -0300

    ajuste de assets

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,7 +1,9 @@
 //= link_tree ../images
-//= link_tree ../javascripts .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../stylesheets .css
 //= link_tree ../stylesheets .js
-//= link_tree ../ .js
-//= link_tree ../../ .js
+//= link_tree ../stylesheets/fonts
+//= link application.js
+//= link controllers/application.js
+//= link controllers/hello_controller.js
+//= link controllers/index.js
