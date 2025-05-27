134ecd666760a165f85f8b8604f3012b18e861f2
Rafael Batista <rafael@devbatista.com>
2025-05-27 11:28:11 -0300

    adicionando assets e views

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,7 @@
 //= link_tree ../images
-//= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
+//= link_tree ../javascripts .js
 //= link_tree ../../../vendor/javascript .js
+//= link_tree ../stylesheets .css
+//= link_tree ../stylesheets .js
+//= link_tree ../ .js
+//= link_tree ../../ .js
