160ef1cb7ed236f23a51ff5c5e8dc2c2fa3448d2
Rafael Batista <rafael@devbatista.com>
2025-05-26 22:28:27 -0300

    adicionando validação na model

--- app/models/client.rb
+++ app/models/client.rb
@@ -6,4 +6,14 @@ class Client < ApplicationRecord
   validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :phone, presence: true, format: { with: /\A\(?\d{2}\)?\s?\d{4,5}-?\d{4}\z/ }
   validates :address, presence: true
+
+  validate :validate_cpf_cnpj
+
+  private
+
+    def validate_cpf_cnpj
+      unless CPF.valid?(cpf_cnpj) || CNPJ.valid?(cpf_cnpj)
+        errors.add(:cpf_cnpj, 'invalid')
+      end
+    end
 end
