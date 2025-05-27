fb9609c77a75cc7833f7da87db73fe6313ebf591
Rafael Batista <rafael@devbatista.com>
2025-05-27 09:13:52 -0300

    adicionando mais validações na model Estimate

--- app/models/estimate.rb
+++ app/models/estimate.rb
@@ -7,5 +7,17 @@ class Estimate < ApplicationRecord
     refused: 2
   }
 
-  validates :description, :value, :status, :due_date, :presence_true
+  validates :description, :value, :status, :due_date, :presence_true, length: { minimum: 5, maximum: 255 }
+  validates :value, presence: true, numericality: { greater_than: 0 }
+  validates :due_date, presence: true
+  
+  validate :due_date_cannot_be_in_the_past
+
+  private
+
+    def due_date_cannot_be_in_the_past
+      if due_date.present? && due_date < Date.today
+        errors.add(:due_date, "cannot be in the past")
+      end
+    end
 end
